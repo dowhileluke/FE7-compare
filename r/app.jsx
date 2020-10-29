196dfcd3cda83eccf926439741f279f7089bdbfe
Luke Davis <dowhileluke@gmail.com>
2020-10-28 20:33:10 -0700

    HeroSelect

--- r/app.jsx
+++ r/app.jsx
@@ -1,22 +1,52 @@
-const { Fragment, useState } = React;
+const { Fragment, useReducer } = React;
 
-function App() {
-  const [state, setState] = useState(false);
-  const toggleState = () => setState(x => !x);
+function toggleInclusion(arr, value) {
+  const result = arr.filter(x => x !== value);
+
+  if (arr.length === result.length) result.push(value);
+
+  return result;
+};
+
+function reducerFn(state, name) {
+  const visibleHeroes = toggleInclusion(state.visibleHeroes, name);
+
+  return {
+    ...state,
+    visibleHeroes,
+  };
+};
+
+const heroNames = characters.map(x => x.name);
+const initState = {
+  heroes: {},
+  visibleHeroes: [],
+};
+const options = [];
+
+characters.forEach(({ name, ...details }) => {
+  initState.heroes[name] = details;
+  options.push();
+});
+
+const HeroSelect = React.memo(({ visibleHeroes, onChange }) => {
+  const availableHeroes = heroNames.filter(x => !visibleHeroes.includes(x));
+  const availableOptions = availableHeroes.map(x => (<option key={x} value={x}>{x}</option>));
 
   return (
-    <React.Fragment>
-      <button onClick={toggleState}>toggle</button>
-      <div id="container">
-        <section className={state ? 'pushed' : ''}>
-          section 1
-        </section>
-        <section className={state ? 'pushed' : ''}>
-          section 2
-        </section>
-      </div>
-    </React.Fragment>
-  )
-}
+    <select value="" onChange={e => onChange(e.target.value)}>
+      <option value="">Select...</option>
+      {availableOptions}
+    </select>
+  );
+});
+
+function App() {
+  const [state, dispatch] = useReducer(reducerFn, initState);
+
+  return (
+    <HeroSelect visibleHeroes={state.visibleHeroes} onChange={dispatch} />
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
